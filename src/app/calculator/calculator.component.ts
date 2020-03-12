564f3240c4d356e4205774d1a2e4fa2552aa1dd0
Mike Gomper <mgomper@avans.nl>
2020-03-12 21:08:43 +0100

    tensor

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import * as tf from '@tensorflow/tfjs';
 
 
 @Component({
@@ -11,39 +12,133 @@ import { Router } from '@angular/router';
 export class CalculatorComponent {
 
   diabetesForm;
+  error = false;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router
   ) {
     this.diabetesForm = new FormGroup({
-      name: new FormControl('null'),
-      glucose: new FormControl('100'),
-      insulin: new FormControl('100'),
-      bloodPressure: new FormControl('80'),
-      bmi: new FormControl('21'),
-      skinThickness: new FormControl('15'),
-      age: new FormControl('30'),
-      pregnancies: new FormControl('1'),
-      email: new FormControl('null'),
+      name: new FormControl('Your name', [
+        Validators.minLength(1),
+        Validators.maxLength(30)
+      ]),
+      glucose: new FormControl('100', [
+        Validators.required,
+        Validators.min(30),
+        Validators.max(350)
+      ]),
+      insulin: new FormControl('100', [
+        Validators.required,
+        Validators.min(5),
+        Validators.max(1000)
+      ]),
+      bloodPressure: new FormControl('80', [
+        Validators.required,
+        Validators.min(20),
+        Validators.max(200)
+      ]),
+      bmi: new FormControl('21', [
+        Validators.required,
+        Validators.min(10),
+        Validators.max(60)
+      ]),
+      skinThickness: new FormControl('15', [
+        Validators.required,
+        Validators.min(5),
+        Validators.max(70)
+      ]),
+      age: new FormControl('30', [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(120)
+      ]),
+      pedigree: new FormControl('1', [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(5)
+      ]),
+      pregnancies: new FormControl('1', [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(20)
+      ]),
     });
   }
+
 
   title = 'noxum';
 
   onSubmit(data) {
-    this.diabetesForm.reset();
-    console.warn('Your order has been submitted', data);
-    this.router.navigate(['/results/' + this.hasDiabetes(data)]);
+    if (this.diabetesForm.valid) {
+      // this.diabetesForm.reset();
+      this.router.navigate(['/results/' + this.hasDiabetes(data)]);
+    } else {
+      this.error = true;
+    }
   }
 
-  hasDiabetes(data) {
-    if(true) {
+  async hasDiabetes(data) {
+
+    const model = await tf.loadLayersModel('https://noxum.nl/model.json');
+
+    const a = tf.tensor([+this.diabetesForm.get('pregnancies').value,
+    +this.diabetesForm.get('glucose').value,
+    +this.diabetesForm.get('bloodPressure').value,
+    +this.diabetesForm.get('skinThickness').value,
+    +this.diabetesForm.get('insulin').value,
+    +this.diabetesForm.get('bmi').value,
+    +this.diabetesForm.get('pedigree').value,
+    +this.diabetesForm.get('age').value]).expandDims();
+
+    console.log(model.predict(a))
+
+    if (true) {
       return true;
     }
     else {
       return false;
     }
+  }
+
+  get name() {
+    return this.diabetesForm.get('name');
+  }
+
+  get glucose() {
+    return this.diabetesForm.get('glucose');
+  }
+
+  get insulin() {
+    return this.diabetesForm.get('insulin');
+  }
+
+  get bloodPressure() {
+    return this.diabetesForm.get('bloodPressure');
+  }
+
+  get skinThickness() {
+    return this.diabetesForm.get('skinThickness');
+  }
+
+  get age() {
+    return this.diabetesForm.get('age');
+  }
+
+  get pregnancies() {
+    return this.diabetesForm.get('pregnancies');
+  }
+
+  get email() {
+    return this.diabetesForm.get('email');
+  }
+
+  get bmi() {
+    return this.diabetesForm.get('bmi');
+  }
+
+  get pedigree() {
+    return this.diabetesForm.get('pedigree');
   }
 
 }
